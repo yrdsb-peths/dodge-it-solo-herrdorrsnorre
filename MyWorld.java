66c6171d68a943b916f00db86731e1726521fcd1
herrdorrsnorre <77855285+herrdorrsnorre@users.noreply.github.com>
2025-04-16 09:34:12 -0400

    added hero and banan

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,10 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        HEro hero = new HEro();
+        addObject(hero, 100, 100);
+        
+        banana banan = new banana();
+        addObject(banan, 600, 100);
     }
 }
