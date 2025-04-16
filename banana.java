87802745e1def583bf75435dfeadf7cc8fecf6a0
herrdorrsnorre <77855285+herrdorrsnorre@users.noreply.github.com>
2025-04-16 13:36:21 -0400

    banana

--- banana.java
+++ banana.java
@@ -12,13 +12,22 @@ public class banana extends Actor
      * Act - do whatever the banana wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public int increase = 1;
     public void act()
     {
         // Add your action code here.
-        move(-3);
+        
+        move(-increase);
         if(getX() <= 0) 
         {
         resetBanana();
+        }
+        
+        if(isTouching(HEro.class)) 
+        {
+            SadFace sadFace = new SadFace();
+            getWorld().addObject(sadFace, 300, 200);
+            getWorld().removeObject(this);
         }
     }
     public void resetBanana() 
@@ -27,10 +36,12 @@ public class banana extends Actor
         if (num == 0) 
         {
             setLocation(600, 100);
+            increase++;
         }
         else 
         {
-        setLocation(600, 300);
+            setLocation(600, 300);
+            increase++;
         }
     }
 }
